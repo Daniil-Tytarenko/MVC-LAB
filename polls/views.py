6f9dd10ba0e6f7f62d95612a845b37e8f64f0912
Daniil-Tytarenko <tytard631_aehit@students.vizja.pl>
2025-03-21 22:37:56 +0100

    commit

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Git is working!!!")
-
-
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
